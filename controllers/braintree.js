9dd47dccbcf84e5e72ee0c99829565522eec98a4
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-03-04 13:45:10 +0530

    update packages versions

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -5,9 +5,9 @@ require('dotenv').config()
 // Connect to braintree payment gateway
 const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
-    merchantId: process.env.BRAINTREE_MERCHANT_ID,
-    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-    privateKey: process.env.BRAINTREE_PRIVATE_KEY
+    merchantId: '4g3x6qdhmpd3mzn3',
+    publicKey: 'jkk693d8kfsghypt',
+    privateKey: 'df50931d50f514607a7eedf230d60023'
 });
 
 // Generate token for user and send to client
